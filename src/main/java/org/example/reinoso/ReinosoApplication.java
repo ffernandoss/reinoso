cbf6e33892a8d3ed7582482fee6d60f1c3da1efb
ffernandoss <fsntama@myuax.com>
2025-01-12 17:15:39 +0100

    Initial commit

---
+++ src/main/java/org/example/reinoso/ReinosoApplication.java
@@ -0,0 +1,13 @@
+package org.example.reinoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReinosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/reinoso/ReinosoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reinoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
