9721cdd1a6b234e178cf6f5192092addf7a13c53
jmartter <150058251+jmartter@users.noreply.github.com>
2025-01-16 11:21:14 +0100

    los comentarios

--- src/main/java/org/example/reinoso/RobotTaskConsumer.java
+++ src/main/java/org/example/reinoso/RobotTaskConsumer.java
@@ -13,24 +13,24 @@ public class RobotTaskConsumer {
     @KafkaListener(topics = "robot-tasks-cortar", groupId = "robot-group")
     public void processCortarTask(Robot robot) {
         logger.info("Procesando tarea de cortar para: " + robot);
-        // Lógica específica para cortar
+
     }
 
     @KafkaListener(topics = "robot-tasks-doblar", groupId = "robot-group")
     public void processDoblarTask(Robot robot) {
         logger.info("Procesando tarea de doblar para: " + robot);
-        // Lógica específica para doblar
+
     }
 
     @KafkaListener(topics = "robot-tasks-apilar", groupId = "robot-group")
     public void processApilarTask(Robot robot) {
         logger.info("Procesando tarea de apilar para: " + robot);
-        // Lógica específica para apilar
+
     }
 
     @KafkaListener(topics = "robot-tasks-transportar", groupId = "robot-group")
     public void processTransportarTask(Robot robot) {
         logger.info("Procesando tarea de transportar para: " + robot);
-        // Lógica específica para transportar
+
     }
 }
